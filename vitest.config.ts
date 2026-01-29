663d5a1ff0cb0a423e75edb516527f7ac5922cb6
Erik C. Forés <soc@erik.cat>
2026-01-30 00:11:27 +0100

    feat(tests): github actions reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      reporters: isGithubActions ? 'github-actions' : 'tree',
       setupFiles: './setupTests.ts',
       environment: 'happy-dom',
     },
