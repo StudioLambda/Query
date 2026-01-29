8f1f00962208ba4b80d66ec7ee8e4ae69b477d61
Erik C. Forés <soc@erik.cat>
2026-01-30 00:15:40 +0100

    fix(tests): revert to default reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      reporters: isGithubActions ? 'github-actions' : 'tree',
       setupFiles: './setupTests.ts',
       environment: 'happy-dom',
     },
